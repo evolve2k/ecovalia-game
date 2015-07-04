d33a710a495315fe4bf1687be4ffad147cfb0f8a
Richie Khoo <evolve2k@gmail.com>
2015-07-04 21:20:29 +0930

    Changes to deck and layout

--- deck.rb
+++ deck.rb
@@ -1,6 +1,23 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+light = '#F3EFE3'
+dark = '#230602'
+
+Squib::Deck.new(cards: 4, layout: %w(hand.yml layout.yml)) do
+  background color: dark
+  deck = xlsx(file: 'data/deck.xlsx')
+  # svg file: deck['Art'], layout: Fauna
+
+  %w(Title Description Snark).each do |key|
+    text str: deck[key], layout: key
+  end
+
+  %w(Defend Health).each do |key|
+    svg file: "assets/icons/#{key.downcase}.svg", layout: "#{key}Icon"
+    text str: deck[key], layout: key
+  end
+
+  save_png prefix: 'even_bigger_'
+  showcase file: 'showcase.png', fill_color: '#0000'
+  hand file: 'hand.png', trim: 37.5, trim_radius: 25, fill_color: '#0000'
 end
