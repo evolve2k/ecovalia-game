6ccaef5fdf152c9d973a00c2041bc50e61805a20
Richie Khoo <evolve2k@gmail.com>
2015-07-22 09:25:02 +0930

    GovHack Unleashed 2015 Just committing it all for now. (Would have committed earlier but I broke my laptop as I left govhack and only just have it back)

--- deck.rb
+++ deck.rb
@@ -3,21 +3,43 @@ require 'squib'
 light = '#F3EFE3'
 dark = '#230602'
 
+# Fauna
 Squib::Deck.new(cards: 4, layout: %w(hand.yml layout.yml)) do
-  background color: dark
-  deck = xlsx(file: 'data/deck.xlsx')
-  # svg file: deck['Art'], layout: Fauna
-
-  %w(Title Description Snark).each do |key|
+  deck = xlsx(file: 'data/fauna.xlsx')
+  svg file: deck['Art'], layout: 'Art'
+  background color: 'brown'
+  %w(Title Category Instructions Facts).each do |key|
     text str: deck[key], layout: key
   end
-
-  %w(Defend Health).each do |key|
-    svg file: "assets/icons/#{key.downcase}.svg", layout: "#{key}Icon"
-    text str: deck[key], layout: key
-  end
-
-  save_png prefix: 'even_bigger_'
-  showcase file: 'showcase.png', fill_color: '#0000'
-  hand file: 'hand.png', trim: 37.5, trim_radius: 25, fill_color: '#0000'
+  save_png prefix: 'fauna_'
+  showcase file: 'fauna_showcase.png', fill_color: '#0000'
+  hand file: 'fauna_hand.png', trim: 37.5, trim_radius: 25, fill_color: '#0000'
 end
+
+# Facilities
+
+# Squib::Deck.new(cards: 4, layout: %w(hand.yml layout.yml)) do
+#   deck = xlsx(file: 'data/facilities.xlsx')
+#   svg file: deck['Art'], layout: 'Art'
+#   background color: 'purple'
+#   %w(Title Category Instructions Facts).each do |key|
+#     text str: deck[key], layout: key
+#   end
+#   save_png prefix: 'facilities_'
+#   showcase file: 'showcase.png', fill_color: '#0000'
+#   hand file: 'hand.png', trim: 37.5, trim_radius: 25, fill_color: '#0000'
+# end
+
+# # Emergency
+
+# Squib::Deck.new(cards: 4, layout: %w(hand.yml layout.yml)) do
+#   deck = xlsx(file: 'data/emergency.xlsx')
+#   svg file: deck['Art'], layout: 'Art'
+#   background color: 'red'
+#   %w(Title Category Instructions Facts).each do |key|
+#     text str: deck[key], layout: key
+#   end
+#   save_png prefix: 'emergency_'
+#   showcase file: 'showcase.png', fill_color: '#0000'
+#   hand file: 'hand.png', trim: 37.5, trim_radius: 25, fill_color: '#0000'
+# end
